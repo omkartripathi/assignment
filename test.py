ae63fbb3ddf1ac9f747373433efeb919c11d4bdf
omkartripathi <68442187+omkartripathi@users.noreply.github.com>
2020-07-18 00:42:43 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
+# 1. assigning elements to different lists.
 
-a = [f"{input()} is the local fun is the"]
-print(a)
+a = [ 1,2,3,4]
+b = [ True , "false", 0 ]
+
+c = [f'{input()} , welcome to coding in python']
+
+# accessing elements from a tuple
+
+d = (1,3,4,5)
+d[nums] # where nums can be any number within length of tuple
+
+# deleting various dictionary items
+
+dict_ = { 1: "one", 2: "two" , 3: "Three"}
+del dict_[key] # here key is the key given in  key-value pair of dictionary
